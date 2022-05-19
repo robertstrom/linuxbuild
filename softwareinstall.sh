1b2c4ecb16ad11256cb218eee739d2a1476852a0
robertstrom <robert.strom@gmail.com>
2022-05-18 21:15:57 -0700

    Update softwareinstall.sh

--- softwareinstall.sh
+++ softwareinstall.sh
@@ -67,7 +67,7 @@ sudo umount /mnt
 sudo apt install -yy gpg gnupg2 gparted htop copyq csvkit exa squashfuse cherrytree pv geany terminator sshfs krusader kdiff3 krename \
 software-properties-common apt-transport-https kompare xxdiff krename dolphin kde-spectacle flameshot remmina discord  vlc vim \
 rhythmbox p7zip-rar p7zip-full uget calibre keepassxc screen pdftk pandoc peek neofetch python3-pip ssh shutter brave-browser \
-typora ffmpeg obs-studio code zsh thefuck
+typora ffmpeg obs-studio code zsh thefuck libimage-exiftool-perl
  
 sudo systemctl enable ssh
 
