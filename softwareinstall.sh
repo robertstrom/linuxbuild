7f1bc74b1f6d41d95284db046c4a630b7c647151
robertstrom <robert.strom@gmail.com>
2022-05-18 20:58:49 -0700

    Update softwareinstall.sh

--- softwareinstall.sh
+++ softwareinstall.sh
@@ -137,6 +137,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm -rf google-chrome-stable_current_amd64.deb
 
+sudo apt update && sudo apt upgrade -y
 
 sudo apt autoremove --purge
 
