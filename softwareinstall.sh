22882651e86b27479473589f0a1f6f8694c3e270
robertstrom <robert.strom@gmail.com>
2022-05-19 06:37:36 -0700

    Update softwareinstall.sh

--- softwareinstall.sh
+++ softwareinstall.sh
@@ -82,7 +82,7 @@ sudo apt install -yy gpg gnupg2 gparted htop copyq csvkit exa squashfuse cherryt
 software-properties-common apt-transport-https kompare xxdiff krename dolphin kde-spectacle flameshot remmina discord  vlc vim \
 rhythmbox p7zip-rar p7zip-full uget calibre keepassxc screen pdftk pandoc peek neofetch python3-pip ssh shutter brave-browser \
 typora ffmpeg obs-studio code zsh thefuck libimage-exiftool-perl catfish doublecmd-common doublecmd-plugins cmatrix wireshark okular \
-archivemount safecopy dcfldd dc3dd xclip
+archivemount safecopy dcfldd dc3dd xclip sipcalc
 
 # SSH Install
 sudo systemctl enable ssh
