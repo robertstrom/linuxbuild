417253b933160bb46df96a5d631926dde10d29d3
robertstrom <robert.strom@gmail.com>
2022-05-18 20:19:50 -0700

    Update softwareinstall.sh

--- softwareinstall.sh
+++ softwareinstall.sh
@@ -3,6 +3,26 @@
 #Command to execute this script from a terminal
 # wget -O - https://raw.githubusercontent.com/robertstrom/linuxbuild/main/softwareinstall.sh | bash
 
+# Add repositories
+# Shutter
+sudo add-apt-repository -y ppa:shutter/ppa
+# Install Typora
+# https://support.typora.io/Typora-on-Linux/
+# or use
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+# Brave Browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+# Install OBS repository
+sudo add-apt-repository ppa:obsproject/obs-studio
+# Install Visual Studio Code repository
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
 sudo apt update && sudo apt upgrade -y
 # sudo apt autoremove --purge
 
@@ -18,7 +38,6 @@ mkdir -p ~/QNAPMyDocs
 mkdir ~/SMBmount
 
 flatpak uninstall com.giuspen.cherrytree -y
-
 
 # Install Joplin
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
@@ -44,41 +63,12 @@ sudo mount ~/AppImages/ObsidianAppImage  -o offset=188392 /mnt
 cp /mnt/usr/share/icons/hicolor/512x512/apps/obsidian.png ~/AppImages/
 sudo umount /mnt
 
-sleep 5
-
-# Install shutter
-sudo add-apt-repository -y ppa:shutter/ppa
-sudo apt update
-sudo apt install shutter -y
-
-sleep 2
-
-# Install Brave Browser
-sudo apt install -y apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
-
-sleep 2
-
-# Install Typora
-# https://support.typora.io/Typora-on-Linux/
-# or use
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
-wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
-# add Typora's repository
-sudo add-apt-repository 'deb https://typora.io/linux ./'
-sudo apt-get update
-# install typora
-sudo apt-get install typora -y
-
-sleep 3
 
 sudo apt install -yy gparted htop copyq csvkit exa squashfuse cherrytree pv geany terminator sshfs krusader kdiff3 krename \
 kompare xxdiff krename dolphin kde-spectacle flameshot remmina discord slack-desktop vlc vim rhythmbox p7zip-rar p7zip-full uget calibre \
-keepassxc screen pdftk pandoc peek neofetch python3-pip ssh
-
+keepassxc screen pdftk pandoc peek neofetch python3-pip ssh shutter brave-browser typora ffmpeg obs-studio curl gpg gnupg2 \
+software-properties-common apt-transport-https code
+ 
 sudo systemctl enable ssh
 
 export qnap='192.168.0.99'
@@ -105,14 +95,6 @@ sudo dpkg -i ~/Downloads/zenmap_7.92-2_all.deb
 ## Updog web server
 ## https://github.com/sc0tfree/updog
 pip3 install updog
-
-# Install OBS
-sudo add-apt-repository ppa:obsproject/obs-studio
-sudo apt update
-sudo apt install ffmpeg obs-studio
-
-sleep 3
-
 
 # Install Wine
 # sudo dpkg --add-architecture i386 
@@ -143,25 +125,14 @@ wget https://raw.githubusercontent.com/robertstrom/oh-my-zsh/main/zshrc-file_del
  git clone https://github.com/MohamedElashri/exa-zsh
  cd -
  
- sleep 1
 
 sudo dpkg --configure -a
-
-# Install Visual Studio Code
-# sudo apt update
-sudo apt install curl gpg gnupg2 software-properties-common apt-transport-https 
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-sudo apt update
-sudo apt install code -y
 
 # sudo apt install google-chrome-stable -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm -rf google-chrome-stable_current_amd64.deb
 
-sleep 1
 
 sudo apt autoremove --purge
 
