7003abbbe3202a289dffcf0551d46baf8fc6d127
robertstrom <robert.strom@gmail.com>
2022-05-25 21:53:46 -0700

    Update softwareinstall.sh

--- softwareinstall.sh
+++ softwareinstall.sh
@@ -187,7 +187,6 @@ rm -rf google-chrome-stable_current_amd64.deb
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt autoremove --purge
 
 # Gnome settings
 # https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/using_the_desktop_environment_in_rhel_8/configuring-gnome-at-low-level_using-the-desktop-environment-in-rhel-8
@@ -209,6 +208,8 @@ sudo cp /usr/share/app-install/desktop/dolphin:org.kde.dolphin.desktop /usr/shar
 sudo bash -c 'echo "Icon=/home/rstrom/Pictures/dolphin_file_manager.png" >> /usr/share/app-install/desktop/dolphin:org.kde.dolphin.desktop'
 
 sudo apt install tshark
+
+sudo apt autoremove --purge
 
 scriptendtime=$(date)
 echo " "
