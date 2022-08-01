c0ed8ca2f00b29785de33534ce80ed1c116e98c5
robertstrom <robert.strom@gmail.com>
2022-07-31 22:37:34 -0700

    Update softwareinstall.sh
    added fzf installation (general-purpose command-line fuzzy finder)

--- softwareinstall.sh
+++ softwareinstall.sh
@@ -113,7 +113,8 @@ rhythmbox p7zip-rar p7zip-full uget calibre keepassxc screen pdftk pandoc peek n
 typora ffmpeg obs-studio code zsh thefuck libimage-exiftool-perl catfish doublecmd-common doublecmd-plugins cmatrix okular \
 xclip sipcalc breeze-icon-theme deja-dup smbclient cifs-utils xsltproc powershell archivemount safecopy dcfldd dc3dd xorriso \
 gnome-tweaks gnome-shell-extensions tesseract-ocr mediainfo mkvtoolnix mkvtoolnix-gui ubuntu-restricted-extras solaar rename datamash \
-rlwrap partitionmanager pigz ncdu mc whois dconf-editor grepcidr speedtest-cli neofetch tmux mpack fonts-powerline font-manager lolcat
+rlwrap partitionmanager pigz ncdu mc whois dconf-editor grepcidr speedtest-cli neofetch tmux mpack fonts-powerline font-manager lolcat \
+fzf
 
 
 
